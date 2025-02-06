c64062b2b2bcd043ddacdd6fb3c99b3a636d660c
AustenJarosz <austenjarosz@gmail.com>
2025-02-05 18:32:32 -0600

    created checks for new transactions

--- src/main/java/com/example/financetracker/transaction/TransactionController.java
+++ src/main/java/com/example/financetracker/transaction/TransactionController.java
@@ -2,6 +2,7 @@ package com.example.financetracker.transaction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
 import java.util.List;
 
@@ -24,6 +25,20 @@ public class TransactionController {
     @PostMapping
     public void registerNewTransaction(@RequestBody Transaction transaction) {
         transactionService.addNewTransaction(transaction);
+    }
+
+    @DeleteMapping(path = "{transactionId}")
+    public void deleteTransaction(@PathVariable ("transactionId") Integer id) {
+        transactionService.deleteTransaction(id);
+    }
+
+    @PutMapping (path = "{transactionId}")
+    public void updateTransaction(
+            @PathVariable ("transactionId") Integer id,
+            @RequestParam(required = false) String merchant,
+            @RequestParam(required = false) Integer amount,
+            @RequestParam(required = false) Date date){
+        transactionService.updateTransaction(id, merchant, amount, date);
     }
 }
 
--- src/main/java/com/example/financetracker/transaction/TransactionService.java
+++ src/main/java/com/example/financetracker/transaction/TransactionService.java
@@ -2,7 +2,10 @@ package com.example.financetracker.transaction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 import java.util.Optional;
 
 import java.time.LocalDate;
@@ -27,10 +30,28 @@ public class TransactionService {
     public void addNewTransaction(Transaction transaction) {
         Optional<Transaction> transactionByMerchant = transactionRepository
                 .findTransactionByMerchant(transaction.getMerchant());
-        if(transactionByMerchant.isPresent()) {
-            throw new IllegalStateException("Transaction from that merchant does not exist");
-        }
+//        if(transactionByMerchant.isPresent()) {
+//            throw new IllegalStateException("Transaction from that merchant does not exist");
+//        }
         transactionRepository.save(transaction);
         System.out.println(transaction);
+    }
+
+    public void deleteTransaction(Integer id) {
+        boolean exists = transactionRepository.existsById(id);
+        if (!exists) {
+            throw new IllegalStateException(
+                    "Transaction with id " + id + " does not exist"
+            );
+        }
+        transactionRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateTransaction(Integer id, String merchant, Integer amount, Date date) {
+        Transaction transaction = transactionRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Transaction with id " + id + " does not exist")
+                );
     }
 }
